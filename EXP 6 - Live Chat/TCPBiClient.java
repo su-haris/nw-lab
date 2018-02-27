050865192a633355972f90e1412d16b594dc8c4a
Suhail <sh.skyinfo@gmail.com>
2018-02-27 20:38:12 +0530

    Live Chat
    Live Chat between clients

---
+++ EXP 6 - Live Chat/TCPBiClient.java
@@ -0,0 +1,78 @@
+import java.net.*;
+import java.io.*;
+
+class TCPBiClient implements Runnable
+{
+	Thread t1, t2;
+	Socket s;
+	BufferedReader br1;
+	OutputStream out;
+	PrintWriter pw;
+	String str1,str2;
+
+	InputStream in;
+	BufferedReader br2;
+	
+	TCPBiClient()
+	{
+		try
+		{
+			t1=new Thread(this);
+			t2=new Thread(this);
+			Socket s=new Socket("localhost",8765);
+		
+			br1=new BufferedReader(new InputStreamReader(System.in));
+			out=s.getOutputStream();
+			pw=new PrintWriter(out,true);
+			in=s.getInputStream();
+			br2=new BufferedReader(new InputStreamReader(in));
+		
+			t1.start();
+			t2.start();
+		}
+		catch (Exception E)
+		{
+		}
+	}
+
+	public void run()
+	{
+		try
+		{
+			if (Thread.currentThread()==t1)
+			{
+				do
+				{
+					str1=br2.readLine();
+					System.out.println("sever says"+str1);
+				}
+				while(!str1.equals("exit"));
+				System.exit(0);
+			}
+
+			else if (Thread.currentThread()==t2)
+			{
+				do
+				{
+					System.out.println("enter your message");
+					str2=br1.readLine();
+					pw.println(str2);
+				}
+				while(!str2.equals("exit"));
+				System.exit(0);
+			}
+		}
+		catch (Exception E)
+		{
+		}
+	}
+}
+
+
+class TCPBiClient1
+{
+	public static void main(String args[])		
+	{
+		TCPBiClient c=new TCPBiClient();
+	}
+}		
---
+++ EXP 6 - Live Chat/TCPLive.java
@@ -0,0 +1,104 @@
+import java.net.*;
+import java.io.*;
+
+class TCPLive implements Runnable
+{
+		Thread t1,t2;
+		Thread[] t;
+		ServerSocket ss;
+		Socket[] s;
+		OutputStream[] out;
+		InputStream[] in;
+		BufferedReader br;
+		BufferedReader[] br2;
+		PrintWriter[] pw;
+		String str2 = " ";
+		int n, i;
+
+		TCPLive()
+		{
+			try
+			{
+				t1=new Thread(this);
+				ss=new ServerSocket(8765);
+				s=new Socket[20];
+				t=new Thread[20];
+				out=new OutputStream[20];
+				in=new InputStream[20];
+				br=new BufferedReader(new InputStreamReader(System.in));
+				br2=new BufferedReader[20];
+				pw=new PrintWriter[20];
+				
+				
+				n=0;
+
+				while(true)
+				{
+						s[n]=ss.accept();
+						out[n]=s[n].getOutputStream();
+						in[n]=s[n].getInputStream();
+						br2[n]=new BufferedReader(new InputStreamReader(in[n]));
+						pw[n]=new PrintWriter(out[n],true);
+						t[n]=new Thread(this);
+						t[n].start();
+						n++;
+						System.out.println("New Client added");
+						System.out.println("No of clients " +n);
+						
+				}
+
+			}
+
+			catch (Exception E)
+			{
+			}
+		}
+
+		public void run()
+		{
+			try
+			{
+				while(true)
+				{
+					int k;
+
+				
+					for(k=0;k<n;k++)
+					{
+						if (Thread.currentThread()==t[k])
+						{
+						
+							str2=br2[k].readLine();
+							for (i=0; i<n; i++)
+							{
+								if (i!=k)
+									pw[i].println("Client"+k+" says "+str2);
+							
+							}
+						
+							if (str2.equals("exit"))
+								System.exit(0);
+						
+						}
+					}
+				}
+			}
+
+			catch (Exception E)
+			{
+			}
+
+		
+		}
+
+}
+
+class TCPLive1
+{
+	public static void main(String args[])		
+	{
+		TCPLive s=new TCPLive();
+	}
+}		
+		
+
